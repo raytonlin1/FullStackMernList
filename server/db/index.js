0abd1e977bca34061d32e60e7153623c3a67885a
Rayton Lin <39320843+raytonlin1@users.noreply.github.com>
2021-05-11 14:27:59 -0400

    Add files via upload

---
+++ server/db/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const mongoURL = 'mongodb+srv://raytonlin1:charmander@cluster0.ha1a8.mongodb.net/Cluster0?retryWrites=true&w=majority'
+mongoose
+    .connect(mongoURL, { useNewUrlParser: true })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+
+const db = mongoose.connection
+
+module.exports = db
---
+++ server/routes/todo-router.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const todoCtrl = require('../controllers/todo-ctrl')
+//const Todo = require('../models/todo-model')
+const router = express.Router()
+
+router.post('/', todoCtrl.createItem)
+router.get('/', todoCtrl.getTodos)
+
+/* 
+router.put('/:id', function(req, res, next) {
+    Todo.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
+  });
+  
+
+  router.delete('/:id', function(req, res, next) {
+    Todo.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
+  });
+*/
+module.exports = router
