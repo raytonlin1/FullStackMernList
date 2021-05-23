b5d342dc0707c49b9b60c520644264b231c08572
Rayton Lin <39320843+raytonlin1@users.noreply.github.com>
2021-05-22 23:18:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import './App.css';
 import Body from './components/Body';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body/>
+      <Router>
+          <Switch>
+              <Route exact path = '/' component = { Body } />
+              <Route exact path = '/header' component = { Header } />
+          </Switch>
+      </Router>
       <Footer />
     </div>
   );
