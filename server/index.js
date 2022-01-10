5471a47312cb869d119274b34b4db3824d0d7693
Rayton Lin <39320843+raytonlin1@users.noreply.github.com>
2022-01-09 23:14:07 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
+//Requires
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+//Database requires
 const db = require('./db')
 const todoRouter = require('./routes/todo-router')
 
+//Start app
 const app = express()
 const apiPort = process.env.PORT || 3000
 
@@ -12,9 +15,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
+//Start database
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-
+//Router for the CRUD commands
 app.use('/', todoRouter)
 
+//Final starting of app
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
